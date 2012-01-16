925e9816c2f9bbfa798bcd7ddf04d4544fbe0fce
Collin <lefeberc@gmail.com>
2012-01-15 20:22:00 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+Design
+======
+
+Models
+======
+
+Team
+    * name
+    * tag
+
+Game
+    * name
+    * abreviation
+
+Member
+    * team_id
+    * real_name
+    * alias
+    * active
+
+TeamMembers
+    * team_id
+    * member_id
+
+Match
+    * team1
+    * team2
+    * score1
+    * score2
+    * approved
+
+MatchPlayerStats
+    * member_id
+    * match_id
+    * kills
+    * deaths
+    * points
+
+Tournament
+    * name
+    * active
+    * game_id
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FragLadder::Application.config.secret_token = '87580e3a64db82c4b35484b9016eaedbc09030d0a0fdd05c6ea7696217d6b4f56de10720bc0f4b913a2808954aadaadfcfba837a247e12a43bb619ee5d940fbb'
